e40fb613f60712b1a0bf7b89f8f402ce172b3da9
Inês Jobino <inesjobino336@gmail.com>
2026-07-28 21:11:06 +0100

    feat(medicine): bug-fix

--- backend/src/modules/medicamento/controller/registrar-medicamento.controller.js
+++ backend/src/modules/medicamento/controller/registrar-medicamento.controller.js
@@ -6,21 +6,27 @@ export const registrarMedicamento = async (req, res) => {
         const principio_ativo = req.body.principio_ativo?.trim()
         const dosagem = req.body.dosagem?.trim()
         const preco_venda = parseFloat(req.body.preco_venda)
-        const categoria_id = parseInt(req.body.categoria_id)
+        const categoria_id = Number(req.body.categoria_id)
         const localizacao_id = Number(req.body.localizacao_id)
         const stock_minimo = Number(req.body.stock_minimo)
 
         if(isNaN(preco_venda)) return res.status(400).json({message: "Forneça um valor válido."})
-
+            
         if (Number.isNaN(categoria_id) || Number.isNaN(localizacao_id) || Number.isNaN(stock_minimo)) {
-            console.log("categoria_id: ",categoria_id,"localizacao_id ",localizacao_id, "stock_minimo", stock_minimo)
-            return res.status(400).json({ message: "Dado inválido." })
-        }
-        console.log("categoria_id: ",categoria_id)
-        if (!nome || !principio_ativo || !preco_venda || !stock_minimo || !categoria_id) {
-            return res.status(400).json({ message: "Campo obrigatório." })
-        }
-    
+                    console.log("categoria_id: ",categoria_id,"localizacao_id ",localizacao_id, "stock_minimo", stock_minimo)
+                    return res.status(400).json({ message: "Dado inválido." })
+                }
+            
+            console.log("categoria_id: ",categoria_id,"localizacao_id ",localizacao_id)
+            if (!nome || !principio_ativo || !preco_venda || !stock_minimo || !categoria_id || !localizacao_id) {
+                return res.status(400).json({ message: "Campo obrigatório." })
+            }
+            
+            
+        /**
+         * ! localizacao_id quando vem vazia dá problema ...como resolver??
+         * *como garantir que a localizacao_id possa vir vazia sendo que não é obrigatório o seu preenchimento?
+         */
 
         const novoMedicamento = await registrarMedicamentoService(nome, principio_ativo, dosagem, preco_venda, stock_minimo, categoria_id, localizacao_id)
 
