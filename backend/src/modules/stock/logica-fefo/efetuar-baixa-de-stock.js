5ddcf7ec94fde87fa2a24a9918be1bd2ad538731
Inês Jobino <inesjobino336@gmail.com>
2026-09-07 18:22:17 +0100

    feat(vendas): segunda funçao para a retirada de medicamentos no stock

---
+++ backend/src/modules/stock/logica-fefo/efetuar-baixa-de-stock.js
@@ -0,0 +1,85 @@
+import prisma from "../../../../prisma/prisma.js"
+
+export const efetuarBaixaDeStock = async (itensParaRetirar) =>
+{
+    /**
+     * ?essa função vai baixar o stock assim que a venda é finalizada, retirando os itens dos lotes ,é ela que mexe na BD
+     * ?ela vai receber os itens ja preparados pela outra funçao ...algo assim:
+     * *[
+        {idVenda: 1,
+            idmed: 0,
+            nome: "mm",
+            lote: [
+                { id: 11, quantidade: 75 },
+                { id: 10, quantidade: 4 }
+            ]
+        },
+        {idVenda: 1,
+            idmed: 15,
+            nome: "mm",
+            lote: [
+                { id: 4, quantidade: 10 }
+            ]
+        }
+    ]
+     */
+    
+    const lotesMovimentados = []
+
+    await prisma.$transaction(async (tx) =>
+    {
+        for (let item of itensParaRetirar) {
+            const lotesParaRetirar = item.lote
+            let lotesRetirados = []
+
+            for (let lote of lotesParaRetirar) {
+                const idLote = lote.id
+                const quantiaParaRetirar = lote.quantidade
+                let loteRetirado = {
+                    id: idLote
+                }
+                const encontrarLote = await tx.lotes.findUnique({
+                    where: {
+                        id: idLote
+                    },
+                })
+                if (encontrarLote) {
+                    const quantiaEncontrada = encontrarLote.qtd_atual
+                    const novaQuantia = quantiaEncontrada - quantiaParaRetirar
+
+                    if(novaQuantia < 0){
+                        console.log("nova quantia"  ,novaQuantia)
+                        throw new Error("Quantia negativa.")
+                    }
+
+                    const retirarMedicamentos = await tx.lotes.update({
+                        where: {
+                            id: idLote
+                        },
+                        data: {
+                            qtd_atual: novaQuantia
+                        },
+                        select: {
+                            id: true,
+                            numero_lote: true,
+                            medicamento: {
+                                select: {
+                                    nome: true
+                                }
+                            },
+                            qtd_atual: true
+                        }
+                    })
+
+                    loteRetirado.qtd = retirarMedicamentos.qtd_atual
+
+                    lotesRetirados.push(loteRetirado)
+                }
+            }
+            lotesMovimentados.push(lotesRetirados)
+        }
+
+    })
+
+    return lotesMovimentados
+}
--- backend/src/modules/stock/logica-fefo/test.js
+++ backend/src/modules/stock/logica-fefo/test.js
@@ -1,8 +1,10 @@
-import { prepararMedicamentosParaVenda } from "./preparar-medicamentos-para-venda.js"
+//import { prepararMedicamentosParaVenda } from "./preparar-medicamentos-para-venda.js"
+
+import { efetuarBaixaDeStock } from "./efetuar-baixa-de-stock.js"
 
 export const test = async(req,res)=>{
     try {
-        const teste = await prepararMedicamentosParaVenda()
+        const teste = await efetuarBaixaDeStock()
         return res.status(200).json(teste)
     } catch (error) {
         console.log(error)
